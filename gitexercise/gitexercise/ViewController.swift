a79979473ece5f59e0a01531bef37c4e74ebf606
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-02 09:58:20 -0700

    Commit to the awesome

--- gitexercise/gitexercise/ViewController.swift
+++ gitexercise/gitexercise/ViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var alert : UIAlertController?
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.generateAlert()
+
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        presentViewController(alert, animated: true) {
+            () -> Void in
+            println("Wowza!")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func generateAlert() {
+        let alertController = UIAlertController(title: "Alerted!", message: "This is going to make a commit suck!", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        var done = UIAlertAction(title: "Done", style: UIAlertActionStyle.Cancel, handler: nil)
+        alertController.addAction(done)
+        
+        self.alert = alertController
     }
 
 
